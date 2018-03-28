15303850f443709b30664da03e513cd8bf9ad47b
Yuhong Sun <yuhongsun96@gmail.com>
2018-03-27 21:46:15 -0700

    Added Play.java

---
+++ Play.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import javax.swing*;
+public class Play{
+  public static void main(String[] args){
+    DisplayGraphics DG = new DisplayGraphics();
+    JFrame frame = new JFrame();
+    frame.add(DG);
+    f.setSize(500, 500);
+    f.setVisible(true);
+  }
+}
